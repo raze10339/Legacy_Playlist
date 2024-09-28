5092fc6b380091a52fc549809cf83d21aeb94181
Moshea14 <matthewoshea00@gmail.com>
2024-09-28 13:58:59 -0400

    my own work

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-('iframe1');
-const iframe2 = document.querySelectorAll('iframe2');
-const paragraph = document.createElement('p1');
+// ('iframe1');
+// const iframe2 = document.querySelectorAll('iframe2');
+// const paragraph = document.createElement('p1');
 
 
-paragraph.textContent = 'Taylor Swift, You Belong With Me, Billie Eilish, What Was I Made For';
+// paragraph.textContent = 'Taylor Swift, You Belong With Me, Billie Eilish, What Was I Made For';
 
 
-iframes.forEach(iframe => {
-    iframe.parentNode.insertBefore(paragraph.cloneNode(true), iframe.nextSibling);
-});
+// iframes.forEach(iframe => {
+//     iframe.parentNode.insertBefore(paragraph.cloneNode(true), iframe.nextSibling);
+// });
 
 const addMusicBtn = document.querySelector('#addMusic');
 const darkModeBtn = document.querySelector('#toggle-btn');
